569508f54edbdc3d320507011b52b9f218cfdbab
Kriti3009 <102670672+Kriti3009@users.noreply.github.com>
2024-04-24 17:44:10 +0530

    added script2.js

---
+++ script2.js
@@ -0,0 +1,9 @@
+// Add functionality to the "Next" button
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.querySelector('.next-button');
+    
+    nextButton.addEventListener('click', function() {
+        // Replace the window.location.href with the URL you want to navigate to
+        window.location.href = 'AThree.html';
+    });
+});
